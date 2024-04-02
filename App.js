0d311c6af172e808f889a0399f10f1633962eddb
Larry Wen <l.wen@griffith.edu.au>
2024-04-02 18:30:53 +1000

    - first commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button } from "react-native";
+import { useState } from "react";
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [arr, setArr] = useState([0]);
+  const [arr2, setArr2] = useState([0]);
+  const incCount = () => setCount((curValue) => curValue + 1);
+  const pushArray = () => {
+    arr.push(arr.length);
+    setArr(arr);
+  };
+  const copyArray = () => {
+    setArr2([...arr2, arr2.length]);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Count: {count}.</Text>
+      <Button title="Increase Count" onPress={incCount} />
+      <Text>Arr: {arr.join(",")}.</Text>
+      <Button title="Push item to arr" onPress={pushArray} />
+      <Text>Arr: {arr2.join(",")}.</Text>
+      <Button title="Copy array with spread" onPress={copyArray} />
     </View>
   );
 }
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
